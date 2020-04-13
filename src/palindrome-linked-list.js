32ca9d294e9eabd9f1f55cce8c54a4e7368bc55e
Chonny <mama.whowho@gmail.com>
2020-04-13 17:16:45 +0800

    Add palindrome linked list

---
+++ src/palindrome-linked-list.js
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+function ListNode(val) {
+    this.val = val
+    this.next = null
+}
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+    if (!head) {
+        return true
+    }
+
+    let numbers = []
+
+    while (head) {
+        numbers.push(head.val)
+        head = head.next
+    }
+
+    let start = 0
+    let end = numbers.length - 1
+
+    while (start < end) {
+        if (numbers[start] !== numbers[end]) {
+            return false
+        }
+
+        start += 1
+        end -= 1
+    }
+
+    return true
+}
+
+const node = new ListNode(1)
+node.next = new ListNode(2)
+
+console.log(isPalindrome(node))
+
+const node1 = new ListNode(1)
+const node2 = new ListNode(2)
+const node3 = new ListNode(2)
+const node4 = new ListNode(1)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+console.log(isPalindrome(node1))
+
+const node5 = new ListNode(-129)
+node5.next = new ListNode(-129)
+
+console.log(isPalindrome(node5))

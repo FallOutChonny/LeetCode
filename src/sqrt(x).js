beda2170b6cbb70fbf3b5f24a0bbbfc4828e48cf
Chonny <mama.whowho@gmail.com>
2020-05-04 13:35:05 +0800

    add sqrt(x)

---
+++ src/sqrt(x).js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x))
+};
+
+console.log(mySqrt(2))
+console.log(mySqrt(8))
+console.log(mySqrt(9))

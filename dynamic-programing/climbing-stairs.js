70091d89388e46af445968921e3664c0b1ff4796
Chonny <mama.whowho@gmail.com>
2020-05-07 14:38:36 +0800

    add climbing stairs

---
+++ dynamic-programing/climbing-stairs.js
@@ -0,0 +1,47 @@
+/**
+ * 70. Climbing Stairs
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Note: Given n will be a positive integer.
+ *
+ * Example 1:
+ *
+ *   Input: 2
+ *   Output: 2
+ *   Explanation: There are two ways to climb to the top.
+ *   1. 1 step + 1 step
+ *   2. 2 steps
+ *
+ * Example 2:
+ *
+ *   Input: 3
+ *   Output: 3
+ *   Explanation: There are three ways to climb to the top.
+ *   1. 1 step + 1 step + 1 step
+ *   2. 1 step + 2 steps
+ *   3. 2 steps + 1 step
+ *
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    if (n <= 1) {
+        return 1
+    }
+
+    let dp = []
+
+    dp[0] = 1 // 1階
+    dp[1] = 2 // 2階
+
+    for (let i = 2; i < n; i++) {
+        dp[i] = dp[i - 2] + dp[i - 1]
+    }
+
+    return dp[dp.length - 1]
+}
+
+console.log(climbStairs(6))

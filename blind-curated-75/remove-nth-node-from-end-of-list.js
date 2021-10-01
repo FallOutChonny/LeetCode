a2752c5ae21b672cc104ccb5d2587cdc7d8d0b87
Chonny <mama.whowho@gmail.com>
2021-10-01 13:07:17 +0800

    remove nth node from end of list

---
+++ blind-curated-75/remove-nth-node-from-end-of-list.js
@@ -0,0 +1,58 @@
+const { print } = require('../utils')
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    if (!head || !head.next) {
+        return null
+    }
+
+    let fast = head
+
+    for (let i = 0; i < n; i++) {
+        fast = fast.next
+    }
+
+    if (!fast) {
+        return head.next
+    }
+
+    let node = head
+    let slow = node
+
+    while (fast.next) {
+        fast = fast.next
+        slow = slow.next
+    }
+
+    slow.next = slow.next.next
+    // print(node)
+
+    return node
+}
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+}
+
+removeNthFromEnd(
+    new ListNode(
+        1,
+        new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+    ),
+    2
+)
+
+removeNthFromEnd(new ListNode(1), 1)
+
+removeNthFromEnd(new ListNode(1, new ListNode(2)), 1)
